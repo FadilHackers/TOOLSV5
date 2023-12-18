642ecaf046f61947c1719072f1a5cc513a58209a
FadilHackers <154044614+FadilHackers@users.noreply.github.com>
2023-12-19 06:01:51 +0700

    Add files via upload

---
+++ .desing/SettingMenu0.sh
@@ -0,0 +1,12 @@
+echo '
+   [ 01 ] >> Logo Setting - d/n
+   [ 02 ] >> Menu Setting - d/n
+   [ 03 ] >> Vibration Setting - on/off
+   [ 04 ] >> Default Loading - on/off
+   [ 05 ] >> Special Opportunities
+   [ 06 ] >> View My Activity
+   [ 07 ] >> Delete My Activity
+   [ 08 ] >> Restore AllHackingTools backup
+   [ 09 ] >> Create AllHackingTools backup
+   [ 10 ] >> Exit System - log out AllHackingTools
+   [ 11 ] >> Back To MainMenu - Back To Main Menu '| lolcat -p 1.8
